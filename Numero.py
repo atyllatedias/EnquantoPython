f515f8a60ee78d2ec13371783a4e38f428a32fff
atyllatedias <113216392+atyllatedias@users.noreply.github.com>
2022-10-03 17:41:31 -0300

    Add files via upload

---
+++ Numero.py
@@ -0,0 +1,11 @@
+soma = int
+x = int
+
+x = int(input("digite um numero (o - sair "))
+soma = 0 
+
+while x != 0:
+    soma = soma + x 
+    x = int(input("digite um numero (0 - sair "))
+
+print("soma vale: " , soma)
